9b9f23e49efd8482155764e34c4d24620962b5d3
Nora de Fitero Teijeira <nora@defitero.com>
2026-02-04 22:51:58 +0100

    on_release hook for input and support for mouse handling

--- src/visualizer/src/core/input.py
+++ src/visualizer/src/core/input.py
@@ -23,6 +23,11 @@ class InputManager:
         EventManager.add_listener(f"on_hold_{key}", callback)
 
     @classmethod
+    def add_listener_on_release(cls, key: keysymdef,
+                                callback: Callable[[], Any]) -> None:
+        EventManager.add_listener(f"on_release_{key}", callback)
+
+    @classmethod
     def remove_listener_on_press(cls, key: keysymdef,
                                  callback: Callable[[], Any]) -> None:
         EventManager.remove_listener(f"on_press_{key}", callback)
@@ -33,10 +38,24 @@ class InputManager:
         EventManager.remove_listener(f"on_hold_{key}", callback)
 
     @classmethod
-    def trigger_key_press(cls, key: keysymdef, param: Any = None) -> None:
+    def remove_listener_on_release(cls, key: keysymdef,
+                                   callback: Callable[[], Any]) -> None:
+        EventManager.remove_listener(f"on_release_{key}", callback)
+
+    @classmethod
+    def trigger_press(cls, key: keysymdef, *args: Any) -> None:
         EventManager.trigger_event(f"on_press_{key}")
         cls.__pressed.add(key)
 
     @classmethod
-    def trigger_key_release(cls, key: keysymdef, param: Any = None) -> None:
-        cls.__pressed.remove(key)
+    def trigger_release(cls, key: keysymdef, *args: list[Any]) -> None:
+        EventManager.trigger_event(f"on_release_{key}")
+        cls.__pressed.discard(key)
+
+    @classmethod
+    def trigger_button_press(cls, button: int, *args: Any) -> None:
+        cls.trigger_press(button + keysymdef.XK_Pointer_Button1 - 1)
+
+    @classmethod
+    def trigger_button_release(cls, button: int, *args: Any) -> None:
+        cls.trigger_release(button + keysymdef.XK_Pointer_Button1 - 1)
